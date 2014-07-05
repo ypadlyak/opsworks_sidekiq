26fd568dd45b90bfc3b07b3a9a63dc83ee4d4af6
Timothy Stott <stott.timothy@gmail.com>
2014-07-05 11:05:23 +0100

    Adds support for platform specific monit config directory

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -61,7 +61,7 @@ node[:deploy].each do |application, deploy|
       end
     end
 
-    template "/etc/monit/conf.d/sidekiq_#{application}.monitrc" do
+    template "#{node[:monit][:conf_dir]}/sidekiq_#{application}.monitrc" do
       mode 0644
       source "sidekiq_monitrc.erb"
       variables({
